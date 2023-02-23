48e45c48493567394ed2e0159981fe8a3149f803
Aparna Bisht <bishta411@gmail.com>
2023-02-23 09:16:39 +0530

    Add files via upload

---
+++ print_number_entered.cpp
@@ -0,0 +1,18 @@
+// C++ program to print a number entered by user
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int number;
+
+    cout<< "Enter an integer: ";
+    cin>> number;
+
+    cout<< "You entered: " << number;
+
+    return 0;
+
+}
