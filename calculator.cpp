4792328456f357ce08eb6baf7affbd1ecad70303
Aparna Bisht <bishta411@gmail.com>
2023-02-24 08:53:11 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,47 @@
+//Program to add, subtract, multiply and divide to numbers taken from user
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    // here, operator is character, a is used for operator
+    char a ; 
+
+    // Inputs are taken as float
+    float num1, num2 ;
+
+    cout << "Enter two operands: \n";
+    cin >> num1 >> num2 ;
+
+    cout << "Enter operator: +, -, *, / \n";
+    cin >> a;
+
+    switch(a)
+        {
+            case '+' :
+            cout << num1 << " + " << num2 << " = " << num1 + num2;
+            break;
+
+            case '-' :
+            cout << num1 << " - " << num2 << " = " << num1 - num2;
+            break;
+
+            case '*' :
+            cout << num1 << " * " << num2 << " = " << num1 * num2;
+            break;
+
+            case '/' :
+            cout << num1 << " / " << num2 << " = " << num1 / num2;
+            break;
+
+            default:
+            cout << "Error! Operator is not correct" ;
+            break;
+
+        }
+
+    return 0;
+
+}
