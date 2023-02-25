18906672cec98e573ed71a82f63f119ca4c6a217
Aparna Bisht <bishta411@gmail.com>
2023-02-25 19:14:39 +0530

    Add files via upload

---
+++ Line_change.cpp
@@ -0,0 +1,17 @@
+// a program in C++ to print a welcome text in a separate line
+
+#include<iostream>
+
+using namespace std;
+ 
+int main()
+{
+    cout << "\n\n Print a welcome text in a separate line: \n";
+
+    cout << "Welcome to\n";
+    cout << "World of Coding!"<< endl;
+    
+    return 0;
+
+
+}
