b9340984e965c0cb518c24c15522d8c2f0637d3a
Aparna Bisht <bishta411@gmail.com>
2023-03-01 10:25:13 +0530

    Add files via upload

---
+++ Copy_string_obj.cpp
@@ -0,0 +1,24 @@
+// Program to copy the string taken from user 
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+
+{
+    string string1, string2;
+
+    cout << "\nCopy the entered string\n\n";
+
+    cout << "Enter the string: \n";
+    getline ( cin, string1 );
+
+    string2 = string1;
+
+    cout << "Input string = "<< string1 << endl;
+
+    cout<< "Output string = "<< string2 << endl;
+
+    return 0;
+}
