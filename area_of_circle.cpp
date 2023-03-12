da032cb6350f23ddd3464829477463fefc6bf42b
Aparna Bisht <bishta411@gmail.com>
2023-03-12 08:42:14 +0530

    Update area_of_circle.cpp

--- area_of_circle.cpp
+++ area_of_circle.cpp
@@ -8,18 +8,18 @@ using namespace std;
 
 int main()
 {
-    double pi = 3.14;                                  // given value to 'pi' which is a variable
-    double radius;                                     // used data type is double
+    double pi = 3.14;                                                   // given value to 'pi' which is a variable
+    double radius;                                                     // used data type is double
     double area ;
 
     cout << "\n-- Compute area of circle --\n" << endl;
 
-    cout << "Enter radius of circle: " << endl;
-    cin >> radius;
+    cout << "Enter radius of circle: " << endl;                            // asking for input
+    cin >> radius;                                                        // read input and stored in variable named radius
 
-    area = pi * radius * radius;
+    area = pi * radius * radius;                                        // calculate area of circle
 
-    cout<< "Area of circle = " << area << endl;
+    cout<< "Area of circle = " << area << endl;                       // output
 
     return 0;
   
